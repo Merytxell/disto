b348416af2367cd2537cbe362d410cee8a18d888
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-15 15:55:57 +0200

    Modify customer entity and Add customer html and repository

--- src/main/java/fr/fms/entities/Customer.java
+++ src/main/java/fr/fms/entities/Customer.java
@@ -9,6 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -20,10 +22,21 @@ public class Customer implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "name must contains alphabetic characters")
     private String name;
+
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "name must contains alphabetic characters")
     private String lastName;
+
+    @Size(min = 10, max = 50)
     private String address;
+
+    @Pattern(regexp = "^[A-Za-z0-9._-]+@[A-Za-z0-9._-]+\\.[a-z]{2,}$",
+            message = "Invalid email format")
     private String email;
+
+    @Pattern(regexp = "^[0-9]{10,14}$", message = "only contains numbers")
     private String phone;
 
 
