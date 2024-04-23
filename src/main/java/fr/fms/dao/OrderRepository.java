a29e13268608a347be5d79c2be0279e8c1e2cd41
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-23 11:19:06 +0200

    add order repository

---
+++ src/main/java/fr/fms/dao/OrderRepository.java
@@ -0,0 +1,10 @@
+package fr.fms.dao;
+
+import fr.fms.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+}
