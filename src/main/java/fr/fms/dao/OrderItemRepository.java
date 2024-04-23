81188a10389ae848c27f43c0b9a41a7d82278d05
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-23 11:34:14 +0200

    Add orderItem repository

--- src/main/java/fr/fms/dao/OrderItemRepository.java
+++ src/main/java/fr/fms/dao/OrderItemRepository.java
@@ -2,10 +2,9 @@ package fr.fms.dao;
 
 import fr.fms.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
-    Optional<OrderItem> findByArticleId(Long articleId);
 }
