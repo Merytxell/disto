6f0e00e1c628516ff90e51b2c87daff6c54541b2
AvilaC <avila.claire@gmail.com>
2024-04-23 14:25:16 +0200

    test de la méthode GetTotalAmountOrder

--- src/test/java/fr/fms/SpringStockMvcSecApplicationTests.java
+++ src/test/java/fr/fms/SpringStockMvcSecApplicationTests.java
@@ -1,13 +1,33 @@
 package fr.fms;
 
+import fr.fms.business.IBusinessImpl;
+import fr.fms.dao.CategoryRepository;
+import fr.fms.entities.Article;
+import fr.fms.entities.Category;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 class SpringStockMvcSecApplicationTests {
 
+	@Autowired
+	IBusinessImpl business;
+
 	@Test
 	void contextLoads() {
+		assertFalse(1==2);
+	}
+	@Test
+	void testGetTotalAmountOrder(){
+		business.addOneArticleToCart(new Article((long)1,"Samsung s8 2024",250, null,null));
+		business.addOneArticleToCart(new Article((long)2,"Samsung s9 2024",350, null,null));
+
+		assertEquals(business.getTotalAmountOrder(),600);
+
 	}
 
 }
