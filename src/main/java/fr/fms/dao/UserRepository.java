5c5efcbeaf565c45d816154ac12118eec5075947
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-23 11:35:26 +0200

    Add request find user by username

--- src/main/java/fr/fms/dao/UserRepository.java
+++ src/main/java/fr/fms/dao/UserRepository.java
@@ -4,4 +4,6 @@ import fr.fms.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
 }
