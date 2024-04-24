2abef306e4b6ebfc756fab838a4b022c7f688f75
AvilaC <avila.claire@gmail.com>
2024-04-24 10:27:23 +0200

    test methode addtocart

--- src/test/java/fr/fms/SpringStockMvcSecApplicationTests.java
+++ src/test/java/fr/fms/SpringStockMvcSecApplicationTests.java
@@ -1,13 +1,49 @@
 package fr.fms;
 
+import fr.fms.business.IBusinessImpl;
+import fr.fms.dao.CategoryRepository;
+import fr.fms.entities.Article;
+import fr.fms.entities.Category;
+import fr.fms.entities.OrderItem;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpringStockMvcSecApplicationTests {
 
+	@Autowired
+	IBusinessImpl business;
+
+
 	@Test
 	void contextLoads() {
+		assertFalse(1==2);
 	}
+	@Test
+	void testGetTotalAmountOrder(){
+		business.addOneArticleToCart(new Article((long)1,"Samsung s8 2024",250, null,null));
+		business.addOneArticleToCart(new Article((long)2,"Samsung s9 2024",350, null,null));
+
+		assertEquals(business.getTotalAmountOrder(),600);
+
+	}
+	@Test
+	void testAddOneArticleToCart() {
+		Article articleToAdd = new Article((long)3, "Samsung s20 2024", 600, null,null);
+		business.addOneArticleToCart(articleToAdd);
+		OrderItem orderItem = new OrderItem();
+		orderItem.setQuantity(1);
+		orderItem.setTotalPrice(600);
+		orderItem.setArticle(articleToAdd);
+		assertTrue(business.getCartContent().contains(orderItem));
+	}
+
+//	@Test
+//	void testremoveOneArticleFromCart(){
+//
+//	}
 
 }
