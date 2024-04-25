d7aa6e26a8a8f36dafb2fd7cf1370e969886318c
AvilaC <avila.claire@gmail.com>
2024-04-25 08:29:37 +0200

    test de delete et article

--- src/test/java/fr/fms/ArticleControllerTest.java
+++ src/test/java/fr/fms/ArticleControllerTest.java
@@ -12,9 +12,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 import java.util.Collections;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -39,10 +44,33 @@ public class ArticleControllerTest {
         Page<Article> emptyPage = new PageImpl<>(Collections.emptyList());
         when(articleRepository.findAll(any(PageRequest.class))).thenReturn(emptyPage);
 
-    mockMvc.perform(get("/index"))
-            .andExpect(status().isOk())
-            .andExpect(view().name("articles"));
+        mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles"));
 
     }
 
+    @Test
+    @WithMockUser(username = "gege", password = "gegedu40!", roles = "admins")
+    void testArticle() throws Exception {
+        mockMvc.perform(get("/article"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("article"));
+
+    }
+
+    @Test
+    @WithMockUser(username = "gege", password = "gegedu40!", roles = "admins")
+    void testDelete() throws Exception {
+        Long articleId = 1L;
+        int page = 1;
+        String kw = "";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/delete")
+                        .param("id", String.valueOf(articleId)).param("page", String.valueOf(page)).param("keyword",kw))
+                        .andExpect(status().is3xxRedirection())
+                        .andExpect(redirectedUrl("/index?page="+page+"&keyword="+kw));
+
+    }
 }
